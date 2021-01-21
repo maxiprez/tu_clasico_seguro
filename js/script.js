13bbadda0cc0028b163bd826954caf7f2a1b2534
Maxi Prez <maxi.prez@gmail.com>
2021-01-21 16:55:45 -0300

    Se creó el objeto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var nombre = prompt("Ingrese su Nombre");
+/*var nombre = prompt("Ingrese su Nombre");
 var apellido = prompt("Ingrese su Apellido");
 var ciudad = prompt("Ingrese su ciudad");
 var edad = parseInt(prompt("Ingrese su Edad"));
@@ -12,4 +12,28 @@ function mensaje () {
 }
 
 //lamo a la funcion:
-mensaje()
+mensaje()*/
+
+let autoMarca = prompt("Ingrese la marca de su vehículo");
+let autoModelo = prompt("Ingrese el modelo de su vehículo");
+let autoAnio = prompt("Ingrese el año de su vehículo");
+let autoDominio = prompt("Ingrese el dominio de su vehículo");
+
+function Auto(autoMarca, autoModelo, autoAnio, autoDominio){
+
+    this.marca = autoMarca;
+    this.modelo = autoModelo;
+    this.anio = autoAnio;
+    this.dominio = autoDominio;
+
+    this.getDatosDelAuto = function(){
+        return `Marca: ${this.marca}, Modelo: ${this.modelo}, Año: ${this.anio}, Dominio: ${this.dominio}`;
+    }
+}
+
+let auto1 = new Auto(autoMarca, autoModelo, autoAnio, autoDominio);
+
+//let auto1 = new Auto("Rambler", "Ambassador", 1966, "SLY-052");
+
+
+console.log("Auto1: ", auto1.getDatosDelAuto());
