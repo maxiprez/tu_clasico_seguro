ffd21ac92ca0958cdc0d4b668454d483fa327ea4
Maxi Prez <maxi.prez@gmail.com>
2021-02-04 16:04:55 -0300

    se agrego mensaje de validacion en todos los campos

--- js/script.js
+++ js/script.js
@@ -4,32 +4,60 @@ let anio = prompt("Ingrese el año de su vehículo:");
 let dominio = prompt("Ingrese el dominio de su vehículo:");
 let importado = prompt("Su clásico es importado?:");*/
 
-//Datos del vehículo:
-let marca = document.getElementById("marca").value;
-let modelo = document.getElementById("modelo").value;
-let anio = document.getElementById("añoAuto").value;
-let dominio = document.getElementById("dominio").value;
+let form = document.getElementById("form");
 
 ///////////////////////////////////////////////////////////////////////////////
-
-//Datos personales y de contacto:
-let nombre = document.getElementById("nombre").value;
-let apellidos = document.getElementById("apellidos").value;
-let fechaDeNacimiento = document.getElementById("fechaDeNacimiento").value;
-let dni = document.getElementById("dni").value;
-let provincia = document.getElementById("provincia").value;
-let localidad = document.getElementById("localidad").value;
-let tel = document.getElementById("tel").value;
-let eMail = document.getElementById("eMail").value;
 
 
 //const IMPORTADO_SI = "SI";
 //const IMPORTADO_NO = "NO";
 
-let getData = function() {
+function validar() {
+    //Datos del vehículo:
+    let marca = document.getElementById("marca").value;
+    let modelo = document.getElementById("modelo").value;
+    let anio = document.getElementById("añoAuto").value;
+    let dominio = document.getElementById("dominio").value;
+    let nombre = document.getElementById("nombre").value;
+    //Datos personales y de contacto:   
+    let apellidos = document.getElementById("apellidos").value;
+    let fechaDeNacimiento = document.getElementById("fechaDeNacimiento").value;
+    let dni = document.getElementById("dni").value;
+    let provincia = document.getElementById("provincia").value;
+    let localidad = document.getElementById("localidad").value;
+    let tel = document.getElementById("tel").value;
+    let eMail = document.getElementById("eMail").value;
 
-    //swal("Good job!", mensajeTexto, "success");
+    expresion = /\w+@\w+\.+[a-z]/;
+
+    if(marca === "" || modelo ==="" || anio === "" || dominio === "" || apellidos === "" ||
+    fechaDeNacimiento === "" || dni === "" || provincia ==="" || localidad === "" || tel === ""
+    || eMail === "") {
+       swal ( "Error!" ,  "Todos los campos son obligatorios" ,  "error" );
+       return false;
+    } else if(nombre.length > 30){
+        swal ( "Error!" ,  "El nombre es muy largo" ,  "error" );
+        return false;
+    }
+    else if(apellidos.length > 80){
+        swal ( "Error!" ,  "Los apellidos son muy largos" ,  "error" );
+        return false;
+    }
+    
+    else if(tel.length > 11){
+        swal ( "Error!" ,  "El teléfono es muy largo" ,  "error" );
+        return false;
+    }
+    else if(isNaN(tel)){
+        swal ( "Error!" ,  "El teléfono ingresado no es un número" ,  "error" );
+        return false;
+    }
+    else if(!expresion.test(eMail)){
+        swal ( "Error!" ,  "Ingrese un correo válido" ,  "error" );
+        return false;
+    }
 }
+
 
 /*
 
